83cb278c075358f9d11091bbf392cb63ae6a87bf
Pranali Rahangdale <156463023+Pranalirahangdale@users.noreply.github.com>
2025-11-25 08:50:57 -0800

    Fix import statement for RecursiveCharacterTextSplitter

--- vdb.py
+++ vdb.py
@@ -6,7 +6,7 @@ from pinecone import Pinecone, ServerlessSpec
 import PyPDF2
 #from langchain_community.document_loaders import PyPDFDirectoryLoader
 import os
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter 
 import google.generativeai as genai
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
